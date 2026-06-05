8b929bfcab1ce3dd43bd917d5160f1b2677067f2
vancuverya-dot <vancuver.ya@gmail.com>
2026-06-06 01:53:45 +0300

    1
    1

--- internal/server/authmiddleware.go
+++ internal/server/authmiddleware.go
@@ -20,14 +20,23 @@ var ErrInvalidToken = errors.New("bad token")
 
 func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var tokenStr string
 
 		cookie, err := r.Cookie("token")
-		if err != nil {
+		if err == nil {
+			tokenStr = cookie.Value
+		}
+
+		if tokenStr == "" {
+			tokenStr = r.Header.Get("Authorization")
+		}
+
+		if tokenStr == "" {
 			w.WriteHeader(http.StatusUnauthorized)
 			return
 		}
 
-		token, err := jwt.ParseWithClaims(cookie.Value, &Claims{}, func(t *jwt.Token) (interface{}, error) {
+		token, err := jwt.ParseWithClaims(tokenStr, &Claims{}, func(t *jwt.Token) (interface{}, error) {
 			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
 				return nil, ErrInvalidToken
 			}
